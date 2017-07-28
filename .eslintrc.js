52cb523024f797073da54f6d496bd79e56f58a73
Paul Guerra <paulrguerra@gmail.com>
2017-07-28 17:37:31 -0400

    work around some ESlint incompatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,8 @@ module.exports = {
   "react/jsx-closing-bracket-location": "warn",
   "no-unused-vars": "warn",
   "no-mixed-operators": ["error", {"allowSamePrecedence": true}],
-  "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}]
+  "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}],
+  "jsx-a11y/href-no-hash": "off", // Added to work around an incompatible peer dependency. see https://github.com/facebookincubator/create-react-app/issues/2631#issuecomment-317207675
+  "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
 }
 };
