f68a54aac8e3dfa8decd40caaec2a4b50ee7bec6
Paul Guerra <paulrguerra@gmail.com>
2017-07-28 17:48:58 -0400

    get a basic server up

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+
+let app = express();
+let server = require('http').createServer(app);
+
+app.use(express.static('dist'));
+app.use(express.static('static'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(3000);
+
