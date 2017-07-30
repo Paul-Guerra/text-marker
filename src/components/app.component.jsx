d83cc8d6100ab318e8a780a42a555a51bb6717fa
Paul Guerra <paulrguerra@gmail.com>
2017-07-29 23:07:00 -0400

    put it on one line for now

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -3,9 +3,7 @@ import Content from './content.component';
 
 export default () => {
   let times = 'once';
-  let text = `i am *_bold_* and **y
-  
-  ou** are not`;
+  let text = 'i am *_bold and underline_* and **you** are not at http://www.google.com . . .  or are you?';
   return (
     <div>
       <h2>My mother made me an app {times}. . . {times.toUpperCase()}!</h2>
