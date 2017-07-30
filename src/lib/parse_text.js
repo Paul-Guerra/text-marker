e2aa1b6a53328b9f81aef37fbaf3d3e3989ce554
Paul Guerra <paulrguerra@gmail.com>
2017-07-29 23:44:28 -0400

    find parts to make dry

--- src/lib/parse_text.js
+++ src/lib/parse_text.js
@@ -6,8 +6,81 @@ function makeSimpleTagRegex(open, close = open) {
 
 let urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/gim;
 // let boldPattern = /\*(?=\S)((?:\*\*|\s+(?:[^\*\s]|\*\*)|[^\s\*])+?)\*(?!\*)/g;
+
+
 let boldPattern = makeSimpleTagRegex('*');
+
+function parseBold(text) {
+  if (!text) return [];
+  let markers = [];
+  let match = boldPattern.exec(text);
+  while (match) {
+    if (match) {
+      markers.push({
+        content: match[1],
+        index: match.index,
+      });
+    }
+    match = boldPattern.exec(text);
+  }
+  return markers;
+}
+
 let underlinePattern = makeSimpleTagRegex('_');
+
+function parseURLs(text) {
+  if (!text) return [];
+  let markers = [];
+  let match = urlPattern.exec(text);
+  while (match) {
+    if (match) {
+      markers.push({
+        content: match[0],
+        index: match.index,
+      });
+    }
+    match = urlPattern.exec(text);
+  }
+  return markers;
+}
+
+function getBoldTokenHandler() {
+  return {
+    pattern: boldPattern,
+    onMatch: function onMatch(match) {
+      return {
+        content: match[1],
+        index: match.index,
+      };
+    }
+  };
+}
+
+
+function getURLTokenHandler() {
+  return {
+    pattern: urlPattern,
+    onMatch: function onMatch(match) {
+      return {
+        content: match[0],
+        index: match.index,
+      };
+    }
+  };
+}
+
+function parseTokens(text, { pattern, onMatch }) {
+  if (!text || !pattern || !onMatch) return [];
+  let markers = [];
+  let match = pattern.exec(text);
+  while (match) {
+    if (match) {
+      markers.push(onMatch(match));
+    }
+    match = pattern.exec(text);
+  }
+  return markers;
+}
 
 class ParsedText {
   constructor({ text }, tokens = []) {
@@ -30,8 +103,7 @@ class ParsedText {
     while (match) {
       if (match) {
         this.markers.push({
-          tag: match[0],
-          content: match[1],
+          content: match[0],
           index: match.index,
         });
       }
@@ -40,9 +112,14 @@ class ParsedText {
   }
 
   buildMarkers() {
-    this.markPattern(boldPattern);
+    // this.markPattern(boldPattern);
+    this.markers.push(...parseTokens(this.normalizedText, getBoldTokenHandler()));
+    this.markers.push(...parseTokens(this.normalizedText, getURLTokenHandler()));
+    // this.markers.push(...parseBold(this.normalizedText));
+    // this.markers.push(...parseURLs(this.normalizedText));
+
     // this.markPattern(underlinePattern);
-    this.markPattern(urlPattern);
+    // this.markPattern(urlPattern);
     console.log(this.markers);
   }
 
@@ -50,6 +127,5 @@ class ParsedText {
     this.buildMarkers();
   }
 }
-
 
 export default ParsedText;
