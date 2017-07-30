821d90e1c092ce9975d1ae90519b4bfa57c24baa
Paul Guerra <paulrguerra@gmail.com>
2017-07-30 16:50:08 -0400

    allow string or regex into keyword factory

--- src/lib/text_parser/tokenhandlers/keyword.js
+++ src/lib/text_parser/tokenhandlers/keyword.js
@@ -3,10 +3,13 @@
  * the factory will produce tokenHandlers for a provided array of keywords
  * */
 
-
-export default function factory(keyword, type = 'KEYWORD') {
+export default function factory(token, type = 'KEYWORD') {
+  let pattern;
+  if (token instanceof RegExp) pattern = token;
+  if (typeof token === 'string') pattern = new RegExp(`${token}`, 'gi');
+  if (!pattern) return {};
   return {
-    pattern: new RegExp(`${keyword}`, 'gi'),
+    pattern,
     onMatch: function onMatch(match) {
       return {
         type,
