9290b9f2b8dc2d092948ccc141b83473ce87a233
Paul Guerra <paulrguerra@gmail.com>
2017-08-04 16:15:45 -0400

    right a better token buffer to handle blocks that span table cells

--- src/lib/text_parser/lexer/utils.js
+++ src/lib/text_parser/lexer/utils.js
@@ -12,12 +12,61 @@ export function getTokensForIndex(index) {
   if (!tokensForIndex[`${index}`]) return [];
   return tokensForIndex[`${index}`].reverse();
 }
+let tokenBuffer = { indexes: {} };
+
+function bufferTokenFor(when = 'BEFORE', selector, token) {
+  let index = `${selector.index}`;
+  let { type, name } = selector;
+
+  if (!token) {
+    console.warn('No token. Bailing.');
+    return;
+  }
+  if (!name && !type && !index) {
+    console.warn('Cannot buffer a token without a name and type or index property. Bailing.');
+    return;
+  }
+  if (index) {
+    if (!tokenBuffer.indexes[index]) tokenBuffer.indexes[index] = {};
+    if (tokenBuffer.indexes[index][when] instanceof Array === false) {
+      tokenBuffer.indexes[index][when] = [];
+    }
+    tokenBuffer.indexes[index][when].push(token);
+    return;
+  }
+
+  if (!tokenBuffer[type]) tokenBuffer[type] = {};
+  if (!tokenBuffer[type][name]) tokenBuffer[type][name] = {};
+  if (tokenBuffer[type][name][when] instanceof Array === false) tokenBuffer[type][name][when] = [];
+  tokenBuffer[type][name][when].push(token);
+}
+
+function flushBufferTokens(now = 'BEFORE', selector) {
+  let index = `${selector.index}`;
+  let { type, name } = selector;
+  if (!name && !type && !index) {
+    console.warn('Cannot flush buffer without a name and type or index property. Bailing.');
+    return [];
+  }
+  if (index) {
+    if (!tokenBuffer.indexes[index]) tokenBuffer.indexes[index] = {};
+    if (tokenBuffer.indexes[index][now] instanceof Array === false) {
+      tokenBuffer.indexes[index][now] = [];
+    }
+    return tokenBuffer.indexes[index][now].reverse();
+  }
+
+  if (!(tokenBuffer[type] && tokenBuffer[type][name])) {
+    return [];
+  }
+  return tokenBuffer[type][name][now];
+}
 
 // Split tokens to eliminate any cases where they overLap
 // eg. turn tokens like this <b>this is <ul>my text</b> over here</u>
 //  into <b>this is <ul>my text</ul></b><u> over here</u>
 // The regular expressions SHOULD be finding only block with matching
-// open and close pairs so there SHOULD NOT be any dangling end blocks, etc
+// open and close pairs so there SHOULD NOT be any dangling end blocks at this point
 export function fixOverlappingBlocks(tokens) {
   let blocks = [];
   let currentBlock;
@@ -35,11 +84,12 @@ export function fixOverlappingBlocks(tokens) {
       return;
     }
 
-    // At this point we have rules out all types except BLOCK_END
+    // At this point we have ruled out all types except BLOCK_END
     currentBlock = blocks[blocks.length - 1] || null;
 
     // if the current open block token doesnt match the current end token
     if (currentBlock && token.name !== currentBlock.name) {
+      // close open blocks w/ different name until you find the match
       for (let i = blocks.length - 1; i >= 0; i--) {
         if (token.name !== blocks[i].name) {
           // create a closing token and push to fixed token 
