5e6ae7d18c58e346724b05794ac1f5b6c98aa134
Paul Guerra <paulrguerra@gmail.com>
2017-08-04 23:27:47 -0400

    test strings

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -3,17 +3,21 @@ import Content from './content.component';
 
 export default () => {
   let times = 'once';
-  // let text = 'i am *bold _and_ underline_* and **you** are /buzz not at http://www.google.com . . .  or are you?';
+  let text = 'i am *bold _and_ underline_* and **you** are /buzz not at http://www.google.com . . .  or are you?';
   // let text = 'i am *bold _and underline* and _ ?';
-  let text = `n*lkk*
+//   let text = `n*lkk*
 
-Step 1	Log*in to Eikon	User should be lo*gged into Eikon
-Step 2	Launch "My Profile & Directory" app	My Profile should be shown
-Step 3	Click on "Edit" button on the top right of profile image section	Image Upload dialog should be shown
-Step 4	From "Set a profile photo" dialog click on "Choose File" image upload and select an image.	The image should be displayed for cropping
-Step 5	Select cropping area in the image and click "Crop" button.	Image should be set as profile image
+// Step 1	Log*in to Eikon	User should be lo*gged into Eikon
+// Step 2	Launch "My Profile & Directory" app	My Profile should be shown
+// Step 3	Click on "Edit" button on the top right of profile image section	Image Upload dialog should be shown
+// Step 4	From "Set a profile photo" dialog click on "Choose File" image upload and select an image.	The image should be displayed for cropping
+// Step 5	Select cropping area in the image and click "Crop" button.	Image should be set as profile image
 
-_plain_`;
+// _plain_`;
+  text = 'n*l_kk*_';
+  // text = 'Step 2	Launch "My Profile & Directory" app	My Profile should be shown';
+  // text = 'Step 1	L*og*in to Eikon	User should be logged into Eikon';
+  // text = 'Step 1	Log*in to Eikon	User should be lo*gged into Eikon';
   return (
     <div>
       <h2>My mother made me an app {times}. . . {times.toUpperCase()}!</h2>
