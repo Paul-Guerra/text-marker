0e6652eb1e555157528499831efe5a6109e0afd6
Paul Guerra <paulrguerra@gmail.com>
2017-08-05 23:26:47 -0400

    update literal object

--- src/lib/text_parser/tokens/types/literal.js
+++ src/lib/text_parser/tokens/types/literal.js
@@ -2,11 +2,11 @@
 // anything left over after getting the markup symbols is a string literal.
 // unlike the BLOCK anf KEYWORD tokenizers there are no more patterns to match 
 // we should have the index before the tokenizer is called
-export default function tokenizer(text, index, name) {
+export default function tokenizer(text, start, name) {
   return {
     name,
     type: 'LITERAL',
     chars: text,
-    index
+    start
   };
 }
