6750b0e5309ff811f5bd717c457cea915a017a33
Paul Guerra <paulrguerra@gmail.com>
2017-08-07 21:45:26 -0400

    Revert "test"
    This reverts commit 94b26be156bf778ea935c6dc2aa3831502978b62.

--- src/lib/text_parser/index.js
+++ src/lib/text_parser/index.js
@@ -1,22 +1,26 @@
 import { pattern as urlPattern, name as urlTokenName } from './tokens/url';
 import { pattern as newlinePattern, name as newlineTokenName } from './tokens/newline';
 import tableTokenizer from './tokens/types/table';
-import lex, { register } from './lexer/';
+import lex from './lexer/';
 import literalTokenizer from './tokens/types/literal';
 import keywordTokenizer from './tokens/types/keyword';
 import textRangeTokenizer from './tokens/types/text_range';
 import blockTokenizer from './tokens/types/block';
 
 export function tokenize(text) {
-  register({ open: '*', close: '*', name: 'BOLD' }, 'BLOCK');
-  register({ open: '_', close: '_', name: 'UNDERLINE' }, 'BLOCK');
-  register({ open: '-', close: '-', name: 'STRIKETHROUGH' }, 'BLOCK');
-  // register('\t', 'TABLE_SEP_VALUE', 100);
-  // register(urlPattern, urlTokenName, 50);
-  // register('/buzz', 'BUZZ');
-  // register('google.com', 'HIGHLIGHT', 20);
-  // register('Step 1', 'HIGHLIGHT', 25);
-  let tokens = lex(text);
+  let tokens = lex(
+    text,
+    [
+      blockTokenizer({ open: '*', close: '*' }, 'BOLD'),
+      blockTokenizer({ open: '_', close: '_' }, 'UNDERLINE'),
+      blockTokenizer({ open: '-', close: '-' }, 'STRIKETHROUGH'),
+      tableTokenizer('\t', 100),
+      textRangeTokenizer(urlPattern, urlTokenName, 50),
+      // keywordTokenizer('/buzz', 'BUZZ'),
+      textRangeTokenizer('google.com', 'HIGHLIGHT', 20),
+      textRangeTokenizer('Step 1', 'HIGHLIGHT', 25)
+    ]
+  );
   // let tokens = lex(text, [tableTokenizer('\t')]);
 }
 
--- src/lib/text_parser/lexer/index.js
+++ src/lib/text_parser/lexer/index.js
@@ -25,67 +25,11 @@ export function printTokens(tokens) {
   console.log(output);
 }
 
-let tokenSignatures = [];
-
-export function register({ open, close, chars, name }, type, priority = 0) {
-  let tokenDef;
-  switch (type) {
-    case 'KEYWORD':
-      tokenDef = { type, chars, name, priority };
-      break;
-    case 'BLOCK':
-      tokenDef = { type, open, close, name, priority };
-      break;
-    default:
-      console.warn(`Warning. Unknown token type ${type}`);
-      return;
-  }
-  tokenSignatures.push(tokenDef);
-  return tokenDef;
-}
-
-let tokensIndex = { offsets: {} };
-
-// for complicated patterns search for them via regex and place 
-// them in a buffer. When the lex parser gets to that index we fush it then
-let patternBuffer = {};
-function indexPattern(pattern, text, signature) {
-  let results = [];
-  let match = pattern.exec(text);
-  while (match) {
-    patternBuffer[`${match.index}`]['BEFORE'] = { signature, match };
-    // tokensIndex.offsets[`${match.index}`] = { signature, match };
-    // tokensIndex[`${signature.name}_${signature.type}`] = { signature, match };
-    match = pattern.exec(text);
-  }
-}
-
-function isToken(char, tokenSignatures) {}
-
-export default function lex(text) {
-  let count = tokenSignatures.length;
+export default function lex(text, tokenizers) {
+  let count = tokenizers.length;
   let tokens = [];
   while (count--) {
-    indexPattern(tokenSignatures[count], text);
-  }
-  let at = 0;
-  let char;
-  let type;
-  let tokenData;
-  while (at < text.length) {
-    // todo: flush tokens before
-    char = text.charAt(at);
-    tokenData = isToken(char, tokenSignatures);
-    if (tokenData) {
-      tokens.push(tokenize(tokenData, char, at, text));
-    }
-    if (tokens[tokens.length - 1].type === 'LITERAL') {
-      //append to token
-    } else {
-      // create literal token and append char
-    }
-    // todo: flush tokens after
-    at += 1; // update if using multiple character tokens
+    tokens.push(...parseTokens(text, tokenizers[count]));
   }
   tokens = tokens.sort((a, b) => {
     if (a.start < b.start) return -1;
