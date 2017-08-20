4fe3990ac423dd3e9c294e94466dea544fa4aa33
Paul Guerra <paulrguerra@gmail.com>
2017-08-20 16:10:53 -0400

    more stubbed data

--- src/__tests__/stubs/tables.stubs.js
+++ src/__tests__/stubs/tables.stubs.js
@@ -1,17 +1,40 @@
+const oneRow = 'Step 1	foo bar baz	lorem ipsum';
+const oneColumn = 'Step 1	';
+const twoColumn = 'Step 1	foo bar baz';
 
 const normal  = `Step 1	foo bar baz	lorem ipsum
 Step 2	foo2 bar2 baz2	lorem2 ipsum2
 Step 3	foo3 bar3 baz3	lorem3 ipsum3
 `;
 
+const markUp  = `Step 1	*foo bar baz*	lorem ipsum
+Step 2	foo2 bar2 baz2	lorem2 *ips*um2
+*Step 3*	foo3 bar3 baz3	lorem3 ipsum3
+`;
 
-const oneRow = 'Step 1	foo bar baz	lorem ipsum';
-const oneColumn = 'Step 1	';
-const twoColumn = 'Step 1	foo bar baz';
+const markUpBetweenCells  = `Step 1	foo bar baz	lorem ipsum
+Step 2	foo2 *bar2 baz2	lorem2 ipsum2*
+Step 3	foo3 bar3 baz3	lorem3 ipsum3
+`;
+
+const overlappingMarkUpInCell  = `Step 1	foo bar baz	lorem ipsum
+Step 2	*foo2 _bar2* baz2_	lorem2 ipsum2
+Step 3	foo3 bar3 baz3	lorem3 ipsum3
+`;
+
+
+const overlappingMarkUpBetweenCells  = `Step 1	foo bar baz	lorem ipsum
+Step 2	*foo2 bar2 _baz2	lorem2* ipsum2_
+Step 3	foo3 bar3 baz3	lorem3 ipsum3
+`;
 
 export default {
   normal,
   oneRow,
   oneColumn,
-  twoColumn
+  twoColumn,
+  markUp,
+  markUpBetweenCells,
+  overlappingMarkUpInCell,
+  overlappingMarkUpBetweenCells
 };
--- src/__tests__/stubs/text.stubs.js
+++ src/__tests__/stubs/text.stubs.js
@@ -5,6 +5,7 @@ export default {
   tables,
   largeText,
   text: 'yar bar foo baz',
+  markupNoWhiteSpace: 'yar b*a*r foo baz',
   mismatchedMarkup: 'i am *bold _and underline* and _ ?',
   linkInMarkup: 'i am *bold http://www.google.com underline* and ?',
   markUp: 'foo *-bar foo-* baz',
