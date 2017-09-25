57cea9a0d73f339928c11b7310afc4c36cc3bed4
Paul Guerra <paulrguerra@gmail.com>
2017-09-25 18:24:49 -0400

    fix bad import

--- src/lib/lexer.js
+++ src/lib/lexer.js
@@ -1,5 +1,5 @@
 import { normalize } from './utils';
-import { insertTableTokens } from './table.utils';
+import { insertTableTokens } from './table_utils';
 import PatternBuffer from './pattern_buffer';
 import Scanner from './scanner';
 
