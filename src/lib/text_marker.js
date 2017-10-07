ce763bf3092aa2ae3358ccc4ab3fd4175d22dfd9
Paul Guerra <paulrguerra@gmail.com>
2017-10-07 16:20:04 -0400

    export middleware until it's in it's own repo

--- src/lib/text_marker.js
+++ src/lib/text_marker.js
@@ -3,14 +3,16 @@ import parse, { tokensToString } from './lexer';
 import range from './types/range';
 import keyword from './types/keyword';
 import block from './types/block';
+import tsv from '../middleware/tsv';
 
 const version = require('../../package.json').version;
 
 export default {
   parse,
-  tokensToString,
   range,
   keyword,
   block,
+  middleware: { tsv },
+  tokensToString,
   version
 };
