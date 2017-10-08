f278c7acd5525cf47902022c48a111411ca0ffa1
Paul Guerra <paulrguerra@gmail.com>
2017-10-08 12:42:04 -0400

    update aslant to remove react references

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
   "extends": "airbnb",
-  "plugins": [
-      "react"
-  ],
+  "plugins": [],
   "parserOptions": {
    "ecmaVersion": 6,
    "sourceType": "module",
    "ecmaFeatures": {
-     "jsx": true,
      "experimentalObjectRestSpread": true
    }
  },
@@ -23,13 +20,8 @@ module.exports = {
   "no-unused-vars": "warn",
   "no-redeclare": "error",
   "no-console": "warn",
-  "react/prop-types": "error",
-  "react/no-multi-comp": "error",
-  "react/jsx-closing-bracket-location": "warn",
   "no-unused-vars": "warn",
   "no-mixed-operators": ["error", {"allowSamePrecedence": true}],
   "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}],
-  "jsx-a11y/href-no-hash": "off", // Added to work around an incompatible peer dependency. see https://github.com/facebookincubator/create-react-app/issues/2631#issuecomment-317207675
-  "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
 }
 };
